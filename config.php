6b302adb6ff5157bea85a823499ada8b2d407b2c
Ukyo97490 <80383797+Ukyo97490@users.noreply.github.com>
2022-03-31 21:10:57 +0400

    Final round before

--- config.php
+++ config.php
@@ -1,27 +1,6 @@
-<!-- <?php
-/**
- * On défini notre fichier de connexion 
- */
-
-// $databaseHost = 'localhost';
-// $databaseName = 'sprint6';
-// $databaseUsername = 'root';
-// $databasePassword = '';
-
-// $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
-
-?> -->
-
 
 <?php
-/* Database connexion */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'sprint6');
- 
-/* Connexion à la base de données */
-$mysqli = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+$mysqli = mysqli_connect("109.234.164.161", $_SERVER['DB_USER'], $_SERVER['DB_PASSWORD'],"sc1lgvu9627_damour-gregory.sprint-06");
  
 // verifier connection
 if($mysqli === false){
